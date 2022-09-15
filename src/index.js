b8dfe46c500318e0e69f8bd6b2bac31e39adb5be
Maksim Ivanovskiy <ivanovskiy.maxim@mail.ru>
2022-09-15 19:44:57 -0400

    1 lesson finished

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom"
-import { createStore, bindActionCreators } from "redux";
+import { createStore } from "redux";
 import { Provider } from "react-redux";
 import reducer from "./reducer";
 
 import App from "./components/app";
 
 const store = createStore(reducer);
-
-
-/* const {inc, dec, rnd} = bindActionCreators(actions, dispatch); */
-
 
 ReactDOM.render(
 	<Provider store={store}>
