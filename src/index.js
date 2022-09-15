95a53616ca8a8dbb48a9f3a58f84c7c06b69e8e9
Maksim Ivanovskiy <ivanovskiy.maxim@mail.ru>
2022-09-15 19:21:10 -0400

    addid redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom"
+import { createStore, bindActionCreators } from "redux";
+import { Provider } from "react-redux";
+import reducer from "./reducer";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import App from "./components/app";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const store = createStore(reducer);
+
+
+/* const {inc, dec, rnd} = bindActionCreators(actions, dispatch); */
+
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App/>
+	</Provider>
+	, document.getElementById('root'));
+
